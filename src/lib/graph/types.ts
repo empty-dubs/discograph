57b177575379d24dd0ab09a0078e0d2e6f176d09
empty-dubs <13712788+empty-dubs@users.noreply.github.com>
2026-08-19 16:33:02 -0700

    update linked master and main release search to include artist name

--- src/lib/graph/types.ts
+++ src/lib/graph/types.ts
@@ -31,9 +31,9 @@ export interface GraphNode {
 	sublabels?: { id: number; name: string }[];
 	artists?: { id: number; name: string }[];
 	tracklist?: { position: string; title: string; duration?: string }[];
-	main_release_info?: { id: number; title: string };
+	main_release_info?: { id: number; title: string; artistName?: string; };
 	main_release?: number;
-	linked_master?: { id: number; title: string };
+	linked_master?: { id: number; title: string; artistName?: string; };
 	notes?: string;
 	labels?: { id: number; name: string; catno?: string }[];
 	credits?: { id: number; name: string; role?: string }[];
