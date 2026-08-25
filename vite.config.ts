97dd141c652472f59c03c01e22ab0dd9e54b307e
empty-dubs <13712788+empty-dubs@users.noreply.github.com>
2026-08-25 02:24:36 -0700

    remove base from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: 'https://empty-dubs.github.io/discograph/',
 	plugins: [tailwindcss(), sveltekit()]
 });
