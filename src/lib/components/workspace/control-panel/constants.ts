4f3d83942cd48c113c14dba290f5478b66629ac9
empty-dubs <13712788+empty-dubs@users.noreply.github.com>
2026-09-23 12:52:59 -0700

    rearrange discover tab

--- src/lib/components/workspace/control-panel/constants.ts
+++ src/lib/components/workspace/control-panel/constants.ts
@@ -1,28 +1,28 @@
 export const CONTROL_PANEL_HELPER_TEXT = {
-	explore: [
-		'Search for an artist, label, release, or master above to begin.',
-		'Once a node has been selected, select an option to add or remove related entities.',
-		'Selecting Collapse children will remove all entities related to the selected node from the graph.',
-		'Selecting Reset graph to this node will remove all entities apart from the selected node from the graph.',
-		'Selecting Clear graph will remove all entities from the graph.'
-	],
 	discover: {
-		entities: [
+		crawl: [
+			'Search for an artist or label above to begin.',
+			'Once a node has been selected, select an option to crawl related entities.',
+			'artist-artist relationships will crawl to related artists via members, groups, and aliases; label-label relationships will crawl to all related labels.',
+			'Select a maximum depth to limit the number of levels of related entities to crawl.',
+			'Select Crawl to begin the crawling. Crawling can be stopped at any time.',
+			'Note: crawling may take some time to complete, and it will eat into your API quota. To avoid running into a rate limit, the maximum crawl speed has been limited match your rate limit.',
+			'Note: more advanced crawl options will be added in the future.'
+		],
+		explore: [
+			'Search for an artist, label, release, or master above to begin.',
+			'Once a node has been selected, select an option to add or remove related entities.',
+			'Selecting Collapse children will remove all entities related to the selected node from the graph.',
+			'Selecting Reset graph to this node will remove all entities apart from the selected node from the graph.',
+			'Selecting Clear graph will remove all entities from the graph.'
+		],
+		related: [
 			'Search for an artist, label, release, or master above to begin.',
 			'Browse aliases, members, labels, credits, and other related entities for the selected node.',
 			'Expand a section to view related entities.',
 			'Click a name to search Discogs and add results to the graph.'
 		]
 	},
-	crawl: [
-		'Search for an artist or label above to begin.',
-		'Once a node has been selected, select an option to crawl related entities.',
-		'artist-artist relationships will crawl to related artists via members, groups, and aliases; label-label relationships will crawl to all related labels.',
-		'Select a maximum depth to limit the number of levels of related entities to crawl.',
-		'Select Crawl to begin the crawling. Crawling can be stopped at any time.',
-		'Note: crawling may take some time to complete, and it will eat into your API quota. To avoid running into a rate limit, the maximum crawl speed has been limited match your rate limit.',
-		'Note: more advanced crawl options will be added in the future.'
-	],
 	settings: {
 		entities: [
 			'Enable/disable node visibility and labels on the graph.',
